1fb6251afd5dc4ebe0d3a6a3fc647e61540a1c95
August Levinson <118256272+920523@users.noreply.github.com>
2024-04-15 10:31:48 +0200

    Finalized api/deck/

--- src/Controller/DeckControllerJSON.php
+++ src/Controller/DeckControllerJSON.php
@@ -16,10 +16,15 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class DeckControllerJSON extends AbstractController
 {
     #[Route("/api/deck", name: "deck_get", methods: ['GET'])]
-    public function deck(): Response
+    public function deck(SessionInterface $session): Response
     {
-        $deck = new DeckOfCardsWithJokers();
-        
+        if ($session->has("deck")) {
+            $deck = $session->get("deck");
+        } else {
+            $deck = new DeckOfCardsWithJokers();
+            $session->set("deck", $deck);
+        }
+
         $data = [
             'deck' => $deck->getAllCardsArray(),
         ];
@@ -33,12 +38,77 @@ class DeckControllerJSON extends AbstractController
         return $response;
     }
     #[Route("/api/deck/shuffle", name: "deck_shuffle", methods: ['POST'])]
-    public function shuffle(): Response
+    public function shuffle(SessionInterface $session): Response
     {
         $deck = new DeckOfCardsWithJokers();
-        
+        $shuffledDeck = clone $deck;
+        $shuffledDeck->shuffleCards();
+        $session->set("shuffled_deck", $shuffledDeck);
+
         $data = [
-            'deck' => $deck->getAllCardsArray(),
+            "shuffledDeck" => $shuffledDeck->getAllCardsArray(),
+        ];
+
+        // return new JsonResponse($data);
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/deck/draw", name: "deck_draw", methods: ['POST'])]
+    public function draw(SessionInterface $session): Response
+    {
+        if ($session->has("deck")) {
+            $deck = $session->get("deck");
+        } else {
+            $deck = new DeckOfCardsWithJokers();
+            $session->set("deck", $deck);
+        }
+
+        $drawnCard = $deck->drawNumber(1);
+        $drawnCardSerialized = [];
+        foreach ($drawnCard as $card) {
+            $drawnCardSerialized[] = $card->toArray();
+        }
+
+        $data = [
+            "drawnCard" => $drawnCardSerialized,
+            "remainingCards" => $deck->getRemainingCards(),
+        ];
+
+        // return new JsonResponse($data);
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/deck/draw/{num<\d+>}", name: "deck_draw_number", methods: ['POST'])]
+    public function drawNum(SessionInterface $session, int $num): Response
+    {
+        if ($session->has("deck")) {
+            $deck = $session->get("deck");
+        } else {
+            $deck = new DeckOfCardsWithJokers();
+            $session->set("deck", $deck);
+        }
+
+        $drawnCard = $deck->drawNumber($num);
+        $drawnCardSerialized = [];
+        if ($drawnCard != null) {
+            foreach ($drawnCard as $card) {
+                $drawnCardSerialized[] = $card->toArray();
+            }
+        }
+
+        $data = [
+            "drawnCard" => $drawnCardSerialized,
+            "remainingCards" => $deck->getRemainingCards(),
         ];
 
         // return new JsonResponse($data);
