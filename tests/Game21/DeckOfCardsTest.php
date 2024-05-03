2c1da02a598eed05345c5f381b6e91f930eb5ae6
August Levinson <118256272+920523@users.noreply.github.com>
2024-05-03 17:23:22 +0200

    Finished unit tests for DeckOfCards class

--- tests/Game21/DeckOfCardsTest.php
+++ tests/Game21/DeckOfCardsTest.php
@@ -314,4 +314,97 @@ class DeckOfCardsTest extends TestCase
             $res
         );
     }
+
+    /**
+     * Construct object and verify that the shuffleDeck() method shuffles the deck.
+     */
+    public function testShuffleCards()
+    {
+        $deck = new DeckOfCards();
+
+        $cardsBeforeShuffle = $deck->getAllCardsArray();
+        $deck->shuffleCards();
+        $cardsAfterShuffle = $deck->getAllCardsArray();
+
+        // Assert that the number of cards is the same before and after shuffle
+        $this->assertCount(count($cardsBeforeShuffle), $cardsAfterShuffle);
+
+        // Assert that the cards are the same before and after shuffle
+        sort($cardsBeforeShuffle);
+        sort($cardsAfterShuffle);
+        $this->assertEquals($cardsBeforeShuffle, $cardsAfterShuffle);
+
+        // Assert that the order of cards has changed
+        $this->assertNotEquals($deck->getAllCardsArray(), $cardsBeforeShuffle);
+    }
+
+    /**
+     * Construct object and verify that the drawCard() 
+     * method returns a Card object and decrements the 
+     * number of cards in the deck by 1.
+     */
+    public function testDrawCard()
+    {
+        $deck = new DeckOfCards();
+        $drawnCard = $deck->drawCard();
+
+        // Deck is decreased by 1
+        $this->assertCount(51, $deck->getAllCards());
+
+        // Drawn card is a Card object
+        $this->assertInstanceOf(Card::class, $drawnCard);
+
+        // When deck is empty, the method returns null
+        $deck->drawNumber(51);
+        $drawnCard = $deck->drawCard();
+        $this->assertNull($drawnCard);
+    }
+
+    /**
+     * Construct object and verify that the drawNumber() 
+     * method returns a Card object and decrements the 
+     * number of cards in the deck by the number in argument (6).
+     */
+    public function testDrawNumber()
+    {
+        $deck = new DeckOfCards();
+        $drawnCards = $deck->drawNumber(6);
+
+        // The returned value is an array
+        $this->assertIsArray($drawnCards);
+
+        // The number of drawn cards is 6
+        $this->assertCount(6, $drawnCards);
+
+        // The deck is decreased by 6
+        $this->assertCount(46, $deck->getAllCards());
+
+        // When deck is empty, the method returns null
+        $deck->drawNumber(46);
+        $drawnCards = $deck->drawNumber(1);
+        $this->assertNull($drawnCards);
+    }
+
+    /**
+     * Construct object and verify that the drawNumber() 
+     * method returns a Card object and decrements the 
+     * number of cards in the deck by the number in argument (6).
+     */
+    public function testGetRemainingCards()
+    {
+        $deck = new DeckOfCards();
+
+        // The returned value is an integer
+        $this->assertIsInt($deck->getRemainingCards());
+        // The number of remaining cards is 38
+        $this->assertEquals(52, $deck->getRemainingCards());
+        
+        $deck->drawNumber(14);
+        // The number of remaining cards is 38
+        $this->assertEquals(38, $deck->getRemainingCards());
+
+        $deck->drawNumber(12);
+        // The number of remaining cards is 34
+        $this->assertEquals(26, $deck->getRemainingCards());
+    }
 }
