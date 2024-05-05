1ed1006309c64ac8fb56b35fc9685e98ec1af6e6
August Levinson <118256272+920523@users.noreply.github.com>
2024-05-05 16:51:21 +0200

    Finished unit tests for CardHand class

---
+++ tests/Game21/CardHandTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Card.
+ */
+class CardHandTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties.
+     */
+    public function testCreateCardHand()
+    {
+        $hand = new CardHand();
+        $this->assertInstanceOf("\App\Card\CardHand", $hand);
+
+        // Test that the hand is empty from the start
+        $res = $hand->getCards();
+        $this->assertEmpty($res);
+
+        // Test that the hand has cards after addCard()
+        $this->assertEmpty($res);
+    }
+
+    /**
+     * Construct object and verify that the object has the expected
+     * properties.
+     */
+    public function testAddCard()
+    {
+        $hand = new CardHand();
+
+        
+        // Test that the hand has cards after addCard()
+        $hand->addCard(new Card("Hearts", "10"));
+        $hand->addCard(new Card("Clubs", "Ace"));
+        $res = $hand->getValues();
+        $this->assertEquals([0 => '10', 1 => 'Ace'], $res);
+    }
+
+}
